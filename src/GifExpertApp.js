3eee7bc4aae7b1a2a87fa827a867ae1df5213f6f
Agustin Clavero <claveroa10@gmail.com>
2022-02-05 06:45:10 -0300

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import  AddCategory  from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState(['One Punch']);
+  
+
+  return (
+       <>
+            <h2 className='title-app animate__animated animate__flipInX'>CLAVE <span className='second-color'>GIFS</span> APP</h2>
+            <AddCategory setCategories={setCategories}  />
+            <hr/>
+            
+            <ol>
+              { 
+              categories.map( category  =>  (
+                <GifGrid 
+                  key={ category }
+                  category={ category }
+                /> 
+                ))
+              }
+            </ol>
+
+
+        </>
+  )
+};
+
+export default GifExpertApp; 
+
+
+
+
+
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category )}&limit=10&api_key=dWLFwuBJE53Q5rXMaS3IzvLFjWS6zeoS`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+      return{
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+      }
+    });
+
+
+    return gifs;
+
+}
